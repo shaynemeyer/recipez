aca7f4c4183e31211660487bb1233abcda30ddd0
shaynemeyer <shayne.meyer@gmail.com>
2015-12-28 06:23:16 -0800

    added CoreData objects and setup project structure.

---
+++ Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  recipez
+//
+//  Created by Shayne on 12/28/15.
+//  Copyright © 2015 Maynesoft LLC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Recipe: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
