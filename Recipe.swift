5732855e0523f5615984b002663d16cd81594e56
shaynemeyer <shayne.meyer@gmail.com>
2015-12-28 06:31:11 -0800

    Added recipes array and get and set recipe image methods.

--- Recipe.swift
+++ Recipe.swift
@@ -8,10 +8,19 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 
 class Recipe: NSManagedObject {
 
-// Insert code here to add functionality to your managed object subclass
+    func setRecipeImage(img: UIImage) {
+        let data = UIImagePNGRepresentation(img)
+        self.image = data
+    }
+    
+    func getRecipeImg() -> UIImage {
+        let img = UIImage(data: self.image!)!
+        return img
+    }
 
 }
--- recipez/ViewController.swift
+++ recipez/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var recipes = [Recipe]()
     
 
     override func viewDidLoad() {
@@ -27,7 +28,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return recipes.count
     }
 
 }
